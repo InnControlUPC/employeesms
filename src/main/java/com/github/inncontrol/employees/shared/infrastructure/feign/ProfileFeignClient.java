3130ea8fd08c00a5c52c4feac177655447ccc0cb
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2025-07-01 15:11:12 -0500

    fixed

--- src/main/java/com/github/inncontrol/employees/shared/infrastructure/feign/ProfileFeignClient.java
+++ src/main/java/com/github/inncontrol/employees/shared/infrastructure/feign/ProfileFeignClient.java
@@ -1,6 +1,7 @@
 package com.github.inncontrol.employees.shared.infrastructure.feign;
 
 import com.github.inncontrol.employees.domain.model.valueobjects.ProfileId;
+import com.github.inncontrol.employees.shared.infrastructure.feign.dto.CreateProfileRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 public interface ProfileFeignClient {
     @PostMapping("/api/v1/profiles")
     @ResponseStatus(HttpStatus.CREATED)
-    ProfileId createProfile(@RequestBody String firstName, String lastName, String phoneNumber, String email);
+    ProfileId createProfile(@RequestBody CreateProfileRequest request);
 
     @GetMapping("api/v1/profiles")
     ProfileId getProfileByEmail(@RequestParam String email);
